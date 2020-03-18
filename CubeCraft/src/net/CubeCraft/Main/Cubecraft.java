288f69a89329ec75187a027269ea0632263d1688
DarkDevss <Dark -darkytzx@gmail.com>
2020-03-18 18:51:30 -0300

    Adição de novos arquivos

--- CubeCraft/src/net/CubeCraft/Main/Cubecraft.java
+++ CubeCraft/src/net/CubeCraft/Main/Cubecraft.java
@@ -1,9 +1,10 @@
 package net.CubeCraft.Main;
 
-public class Cubecraft {
+public class Cubecraft 
+{
 	
-	public static void OI() {
-		
+	public static void main(String[] args) {
+		new Screen("Cubecraft - version 1.0 Alpha", 800, 500);
 	}
 	
 }
---
+++ CubeCraft/src/net/CubeCraft/Main/Screen.java
@@ -0,0 +1,17 @@
+package net.CubeCraft.Main;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class Screen extends JFrame 
+{
+
+	public Screen(String title, int width, int height) {
+		setTitle(title);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(width, height);
+		setLocationRelativeTo(null);
+		setVisible(true);
+	}
+	
+}
